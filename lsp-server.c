342aac35bfc560895d749625036ec95d3f43c3a1
t101o <teodorsdrazlovskis@gmail.com>
2021-05-07 21:31:51 +0300

    Fixed connection stuff and a typo

--- lsp-server.c
+++ lsp-server.c
@@ -13,6 +13,9 @@
 
 #include "packets.h"
 
+// For now port is 1234
+#define PORT 1234
+
 int timer() {
 	
 	
@@ -60,24 +63,16 @@ void player_connect() {
     char *hello = "Hello from server";
        
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
        
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
        
-    // Forcefully attaching socket to the port 8080
     if (bind(server_fd, (struct sockaddr *)&address, 
                                  sizeof(address))<0)
     {
@@ -95,10 +90,14 @@ void player_connect() {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
+    while (1) {
+    valread = read(new_socket, buffer, 1024);
+    printf("%s\n", buffer);
     send(new_socket , hello , strlen(hello) , 0 );
     printf("Hello message sent\n");
+    // After using buffer, it needs to be cleared
+    memset(buffer, 0, 1024);
+    }
     return 0;
 }
 
